27f7f4361b996440a353b8c9e4b5f5c8ad38b105
zahr2310 <zeroto585@gmail.com>
2026-06-25 17:50:09 +0200

    add scripts/commands/restart.js

---
+++ scripts/commands/restart.js
@@ -0,0 +1,20 @@
+module.exports.config = {
+	name: "restart",
+	version: "7.0.0",
+	permission: 2,
+	credits: "ryuko",
+	prefix: false,
+  premium: false,
+	description: "restart bot system",
+	category: "admin",
+	usages: "",
+	cooldowns: 0,
+	dependencies: {
+		"process": ""
+	}
+};
+module.exports.run = async function({ api, event, args, Threads, Users, Currencies, models }) {
+  const process = require("process");
+  const { threadID, messageID } = event;
+  api.sendMessage(`restarting ${global.config.BOTNAME} ai, please be patient.`, threadID, ()=> process.exit(1));
+}
